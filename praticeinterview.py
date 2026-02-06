4dedbeb7ff5549479710cd5c88a2b4dcb5ad1d31
Rohit Kumar <pottamrohitks12@gmail.com>
2026-02-06 23:21:51 +0530

    learning

--- praticeinterview.py
+++ praticeinterview.py
@@ -22,3 +22,18 @@ for i in range(1, 11):
 def add(a, b):
     return a + b
 print("Addition:", add(5, 3))
+
+
+#Factorial using function
+def factorial(n):
+    f = 1
+    for i in range(1, n+1):
+        f *= i
+    return f
+print(factorial(5))
+
+
+#list programs
+#Find largest number in a list
+numbers = [3, 5, 2, 8, 1]
+print ("Max:",max(numbers))

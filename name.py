ae76e6ea75f4e3105a5a330d80a72e03b0a5f6c9
Rohit Kumar <pottamrohitks12@gmail.com>
2026-02-14 10:14:05 +0530

    name

---
+++ name.py
@@ -0,0 +1,6 @@
+def stu(*name):
+  print("Suprabhat",name)
+  print("First Name:", name[0])
+  print("Last Name:", name[-2])
+  
+stu("Madhav","Shyam","Vasudev","Krishna","Devkinandan","Gopal","Dwarkadish")

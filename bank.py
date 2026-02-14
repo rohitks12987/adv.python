cfc58d479ff828e29fb975ddc1494dd8604d7964
Rohit Kumar <pottamrohitks12@gmail.com>
2026-02-14 09:32:50 +0530

    bank program

---
+++ bank.py
@@ -0,0 +1,4 @@
+def bank(customer_name,age,account_no,branch,ifsc_code,balance,account_type):
+  print(f"Good Morning !!\nCustomer Name:{customer_name}\nAge is:{age}\nAccount Number:{account_no}\nBranch:{branch}\nIFSC Code:{ifsc_code}\nBalance:{balance}\nAccount Type:{account_type}")
+
+bank("Rohit Kumar",21,34567235,"Chikiti Pentho","Union Bank Of India",25000,"Savings")

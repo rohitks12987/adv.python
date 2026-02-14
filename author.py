5cabadd50ce3ed1294d4e99bde59d357fb04148c
Rohit Kumar <pottamrohitks12@gmail.com>
2026-02-14 09:51:48 +0530

    author details

---
+++ author.py
@@ -0,0 +1,4 @@
+def book(book_name,author_name,price,quantity):
+  print(f"Book Name:{book_name}\nAuthor Name:{author_name}\nPrice:{price}\nQuantity:{quantity}")
+
+book(book_name="Merchant of Venice ",author_name="Rohit Kumar Subudhi",price=25000,quantity=2)

b91287dc188d8097940f839574e1a7f02493968c
Rohit Kumar <pottamrohitks12@gmail.com>
2026-02-14 09:27:30 +0530

    argument function

---
+++ argument function.py
@@ -0,0 +1,4 @@
+def stu(name,age,rollno,sec):
+  print(f"Good Morning ji !! \nHello My name is {name}\n My age is {age} \n My roll no. is {rollno} \n My sec is {sec}")
+
+stu("Rohit Kumar",21,543,"I")

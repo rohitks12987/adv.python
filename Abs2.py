ef21d1a4fde4e6f5f635cee03c701e29bc51374c
Rohit Kumar <pottamrohitks12@gmail.com>
2026-03-05 23:38:42 +0530

    hospital.py

--- Abs2.py
+++ Abs2.py
@@ -1,4 +1,3 @@
-#in there i call the object of the Abs2 file and call the method to display the details of the car and truck class which is inherited from the vehicle class and have the method to display the details of the vehicle class declaring by the for loop and creating the object of the car and truck class and calling
 import Abs2 as Abs
 car = Abs.Car
 bike = Abs.Bike
